7f8b8e460602d022a8b9d33c39bf30dc37547dab
richardsunny2015 <richardsunny2015@gmail.com>
2018-08-01 11:11:31 -0400

    finish(checkPerm): checks to see if one string is a permutation of the other

--- cracking-the-coding-interview/arraysAndStrings.js
+++ cracking-the-coding-interview/arraysAndStrings.js
@@ -7,7 +7,27 @@ const isUnique = (str) => {
     return Object.keys(hashTable).length === str.length;
 };
 
+const checkPerm = (str1, str2) => {
+    if (str1.length !== str2.length) return false;
+    const hashTable1 = {};
+    const hashTable2 = {};
+    for (let i = 0; i < str1.length; i++) {
+        if (hashTable1.hasOwnProperty(str1[i])) hashTable1[str1[i]]++;
+        else hashTable1[str1[i]] = 1;
+        if (hashTable2.hasOwnProperty(str2[i])) hashTable2[str2[i]]++;
+        else hashTable2[str2[i]] = 1;
+    }
+    const hashTable1Keys = Object.keys(hashTable1);
+    const hashTable2Keys = Object.keys(hashTable2);
+    if (hashTable1Keys.length !== hashTable2Keys.length) return false;
+    for (let j = 0; j < hashTable1Keys.length; j++) {
+        if (hashTable1[hashTable1Keys[j]] !== hashTable2[hashTable1Keys[j]]) return false;
+    }
+    return true;
+}
+
 
 module.exports = {
-    isUnique
+    isUnique,
+    checkPerm
 }
--- test/arraysAndStrings.spec.js
+++ test/arraysAndStrings.spec.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai');
-const {isUnique} = require('../cracking-the-coding-interview/arraysAndStrings');
+const {isUnique, checkPerm} = require('../cracking-the-coding-interview/arraysAndStrings');
 
 describe('isUnique', () => {
     it('should return a boolean', () => {
@@ -13,5 +13,20 @@ describe('isUnique', () => {
     it('should return false if there are repeating characters', () => {
         expect(isUnique('hahahahahaah')).to.be.false;
         expect(isUnique('this is a test')).to.be.false;
+    })
+})
+
+describe('checkPerm', () => {
+    it('returns a boolean', () => {
+        expect(checkPerm('yah', 'woo')).to.be.a('boolean');
+    })
+    it('returns true if one string is a permutation of the other', () => {
+        expect(checkPerm('hola', 'loha')).to.be.true;
+        expect(checkPerm('haha', 'ahah')).to.be.true;
+        expect(checkPerm('super duper', 'druspe rupe')).to.be.true;
+    })
+    it('returns false if one is not a permutation of the other', () => {
+        expect(checkPerm('hola', 'nope')).to.be.false;
+        expect(checkPerm('yayaya', ' ')).to.be.false;
     })
 })
