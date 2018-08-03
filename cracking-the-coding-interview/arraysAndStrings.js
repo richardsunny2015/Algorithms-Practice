9516f25fac0d1e8529ac5cf641d2cb5683e3aad0
Richard Lanchez <richardsunny2015@gmail.com>
2018-08-03 08:27:05 -0400

    Arrays and strings #1 (#2)
    * finished(isUnique): isUnique is an algo that checks for unique characters in a string
    
    * finish(checkPerm): checks to see if one string is a permutation of the other
    
    * fin(urlify): replaces spaces with %20
    
    * fin(paliPerm): returns boolean for possible palindrom permutations
    
    * fin(oneAway): checks to see if two strings are one edit away from each other
    
    * test(strCompress): wrote tests for strCompress
    
    * fin(strCompress): strCompress passes tests, but seems logically complicated. Will fix in the future
    
    * feat(test): test script now has watch flag

---
+++ cracking-the-coding-interview/arraysAndStrings.js
@@ -0,0 +1,94 @@
+const isUnique = str => {
+  const hashTable = {};
+  for (let i = 0; i < str.length; i++) {
+    if (hashTable.hasOwnProperty(str[i])) hashTable[str[i]]++;
+    else hashTable[str[i]] = 1;
+  }
+  return Object.keys(hashTable).length === str.length;
+};
+
+const checkPerm = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+  const hashTable1 = {};
+  const hashTable2 = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (hashTable1.hasOwnProperty(str1[i])) hashTable1[str1[i]]++;
+    else hashTable1[str1[i]] = 1;
+    if (hashTable2.hasOwnProperty(str2[i])) hashTable2[str2[i]]++;
+    else hashTable2[str2[i]] = 1;
+  }
+  const hashTable1Keys = Object.keys(hashTable1);
+  const hashTable2Keys = Object.keys(hashTable2);
+  if (hashTable1Keys.length !== hashTable2Keys.length) return false;
+  for (let j = 0; j < hashTable1Keys.length; j++) {
+    if (hashTable1[hashTable1Keys[j]] !== hashTable2[hashTable1Keys[j]]) return false;
+  }
+  return true;
+};
+
+const urlify = str => {
+  // return str.split(' ').join('%20'); easy solution
+  let retStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') retStr += '%20';
+    else retStr += str[i];
+  }
+  return retStr;
+};
+
+const paliPerm = str => {
+  const hashTable = {};
+  for (let i = 0; i < str.length; i++) {
+    if (hashTable.hasOwnProperty(str[i])) hashTable[str[i]]++;
+    else hashTable[str[i]] = 1;
+  }
+  return (
+    Object.keys(hashTable).filter(key => hashTable[key] % 2 !== 0).length < 2
+  );
+};
+
+const oneAway = (str, fixedStr) => {
+  let longest = str.length > fixedStr.length ? str : fixedStr;
+  let shortest = str.length <= fixedStr.length ? str : fixedStr;
+  for (let i = 0; i < longest.length; i++) {
+    if (longest[i] !== shortest[i]) {
+      if (longest.length - 1 === shortest.length) {
+        shortest = shortest.slice(0, i) + longest[i] + shortest.slice(i);
+      } else if (longest.length === shortest.length) {
+        shortest = shortest.slice(0, i) + longest[i] + shortest.slice(i + 1);
+      }
+      return shortest === longest;
+    }
+  }
+  return false;
+};
+
+const strCompress = str => {
+  let retStr = '';
+  let letterCount = 0;
+  let currenctCharacter = str[0];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === currenctCharacter) {
+      letterCount++;
+    } else {
+      retStr +=
+        letterCount > 1 ? currenctCharacter + letterCount : currenctCharacter;
+      letterCount = 1;
+      currenctCharacter = str[i];
+    }
+    if (i + 1 === str.length) {
+      retStr +=
+        letterCount > 1 ? currenctCharacter + letterCount : currenctCharacter;
+    }
+  }
+  return retStr;
+};
+
+module.exports = {
+  isUnique,
+  checkPerm,
+  urlify,
+  paliPerm,
+  oneAway,
+  strCompress
+};
---
+++ test/arraysAndStrings.spec.js
@@ -0,0 +1,117 @@
+const { expect } = require('chai');
+const {
+  isUnique,
+  checkPerm,
+  urlify,
+  paliPerm,
+  oneAway,
+  strCompress
+} = require('../cracking-the-coding-interview/arraysAndStrings');
+
+xdescribe('isUnique', () => {
+  it('should return a boolean', () => {
+    expect(isUnique('test')).to.be.a('boolean');
+  });
+  it('should return true if characters are unique', () => {
+    expect(isUnique('bop it')).to.be.true;
+    expect(isUnique('omg')).to.be.be.true;
+    expect(isUnique('hola')).to.be.be.true;
+  });
+  it('should return false if there are repeating characters', () => {
+    expect(isUnique('hahahahahaah')).to.be.false;
+    expect(isUnique('this is a test')).to.be.false;
+  });
+});
+
+xdescribe('checkPerm', () => {
+  it('returns a boolean', () => {
+    expect(checkPerm('yah', 'woo')).to.be.a('boolean');
+  });
+  it('returns true if one string is a permutation of the other', () => {
+    expect(checkPerm('hola', 'loha')).to.be.true;
+    expect(checkPerm('haha', 'ahah')).to.be.true;
+    expect(checkPerm('super duper', 'druspe rupe')).to.be.true;
+  });
+  it('returns false if one is not a permutation of the other', () => {
+    expect(checkPerm('hola', 'nope')).to.be.false;
+    expect(checkPerm('yayaya', ' ')).to.be.false;
+  });
+});
+
+xdescribe('urlify', () => {
+  it('returns a string', () => {
+    expect(urlify('yah girl')).to.be.a('string');
+  });
+  it('returns URLified string for two words', () => {
+    expect(urlify('hello bob')).to.equal('hello%20bob');
+    expect(urlify('goodbye sam')).to.equal('goodbye%20sam');
+  });
+  it('returns URLified version of three or more words', () => {
+    expect(urlify('see you later alligator')).to.equal(
+      'see%20you%20later%20alligator'
+    );
+    expect(
+      urlify('been far too long in the midnight sea oh what is becoming of me')
+    ).to.equal(
+      'been%20far%20too%20long%20in%20the%20midnight%20sea%20oh%20what%20is%20becoming%20of%20me'
+    );
+  });
+});
+
+xdescribe('paliPerm', () => {
+  it('returns a boolean', () => {
+    expect(paliPerm('yah')).to.be.a('boolean');
+  });
+  it('returns true for strings that could be palindromes', () => {
+    expect(paliPerm('rraacce')).to.be.true;
+    expect(paliPerm('sttsa')).to.be.true;
+  });
+  it('returns false for non-palindrome permutations', () => {
+    expect(paliPerm('hello')).to.be.false;
+    expect(paliPerm('goodbye')).to.be.false;
+  });
+});
+
+xdescribe('oneAway', () => {
+  it('returns a boolean', () => {
+    expect(oneAway('beat', 'bet')).to.be.a('boolean');
+  });
+  it('returns true for strings that need one character insert', () => {
+    expect(oneAway('bet', 'beat')).to.be.true;
+    expect(oneAway('yah', 'yeah')).to.be.true;
+  });
+  it('returns true for strings that need one character removed', () => {
+    expect(oneAway('beat', 'bet')).to.be.true;
+    expect(oneAway('yeah', 'yah')).to.be.true;
+  });
+  it('returns true for strings that need one character replaced', () => {
+    expect(oneAway('beat', 'best')).to.be.true;
+    expect(oneAway('yeas', 'yeah')).to.be.true;
+  });
+  it('returns false for strings that need more than one edit', () => {
+    expect(oneAway('beat', 'beat it')).to.be.false;
+    expect(oneAway('howdy', 'hello')).to.be.false;
+  });
+});
+
+describe('strCompress', () => {
+    it('returns a string', () => {
+        expect(strCompress('abc')).to.be.a('string');
+    })
+    it('returns a compressed string with non-repeating characters', () => {
+        expect(strCompress('aaaaaabbbbbcccc')).to.equal('a6b5c4');
+        expect(strCompress('xxxyyzzzz')).to.equal('x3y2z4');
+    })
+    it('returns a compressed string with repeating characters', () => {
+        expect(strCompress('aaabbaaaa')).to.equal('a3b2a4');
+        expect(strCompress('yyyzzzxxxyy')).to.equal('y3z3x3y2');
+    })
+    it('doesn\'t compress single characters', () => {
+        expect(strCompress('abbbccc')).to.equal('ab3c3');
+        expect(strCompress('deeeffdrrh')).to.equal('de3f2dr2h');
+    })
+    it('returns original string if there is no compression', () => {
+        expect(strCompress('abc')).to.equal('abc');
+        expect(strCompress('abcdefghijklmnopqrstuvwxyz')).to.equal('abcdefghijklmnopqrstuvwxyz');
+    })
+})
