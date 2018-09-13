bb55fb7687f6755838af63478c7ad2e0f78fd90b
Richard Lanchez <richardsunny2015@gmail.com>
2018-09-13 12:25:27 -0400

    feat(recursionAndDynamicProgramming.js): mergeSortedArrays passes tests (#22)

--- cracking-the-coding-interview/recursionAndDynamicProgramming.js
+++ cracking-the-coding-interview/recursionAndDynamicProgramming.js
@@ -1,3 +1,12 @@
-const mergeSortedArrays = () => {}
+const mergeSortedArrays = (arr1, arr2) => {
+  return (function merge(arr1, arr2, mergedArray) {
+    if (!arr1.length || !arr2.length) {
+      let leftovers = arr1.length ? arr1 : arr2;
+      return mergedArray.concat(leftovers);
+    }
+    if (arr1[0] <= arr2[0]) {return merge(arr1.slice(1), arr2, mergedArray.concat([arr1[0]]));}
+    else {return merge(arr1, arr2.slice(1), mergedArray.concat([arr2[0]]));}
+  })(arr1, arr2, []);
+};
 
 module.exports = { mergeSortedArrays };
