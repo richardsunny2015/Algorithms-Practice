95cadf2f2f161be31f79334e89929909c48e982f
Richard Lanchez <richardsunny2015@gmail.com>
2018-09-13 12:17:28 -0400

    Merge sorted arrays tests #19 (#21)
    * add(recursionAndDynamicProgramming): added js and spec files
    
    * test(recursionAndDynamicProgramming): added tests for mergeSortedArrays

---
+++ cracking-the-coding-interview/recursionAndDynamicProgramming.js
@@ -0,0 +1,3 @@
+const mergeSortedArrays = () => {}
+
+module.exports = { mergeSortedArrays };
---
+++ test/recursionAndDynamicProgramming.spec.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+const {
+  mergeSortedArrays
+} = require('../cracking-the-coding-interview/recursionAndDynamicProgramming');
+
+describe('mergeSortedArrays', () => {
+  it('is a function', () => {
+    expect(mergeSortedArrays).to.be.a('function');
+  });
+  it('returns an array', () => {
+    expect(mergeSortedArrays([], [])).to.be.an('array');
+  });
+  it('merges two sorted arrays with different values', () => {
+    expect(mergeSortedArrays([1, 3, 5], [2, 4, 6])).to.eql([1, 2, 3, 4, 5, 6]);
+  });
+  it('merges arrays with some similar values', () => {
+    expect(mergeSortedArrays([1, 2, 5, 7], [1, 3, 5, 8])).to.eql([
+      1,
+      1,
+      2,
+      3,
+      5,
+      5,
+      7,
+      8
+    ]);
+  });
+  it('merges two uneven arrays', () => {
+    expect(mergeSortedArrays([1, 8, 9], [2, 4, 10, 12, 15])).to.eql([
+      1,
+      2,
+      4,
+      8,
+      9,
+      10,
+      12,
+      15
+    ]);
+  });
+});
