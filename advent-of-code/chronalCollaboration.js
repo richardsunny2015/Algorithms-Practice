43aaf31a02fc13f7e4bce7c25ccc520b039110b5
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-04 07:58:59 -0500

    feat(chronalCollaboration): Finish writing algorithm

--- advent-of-code/chronalCollaboration.js
+++ advent-of-code/chronalCollaboration.js
@@ -1,3 +1,18 @@
-const chronalCollaboration = () => {}
+const chronalCollaboration = str => {
+  if (!str) return 0;
+  const addOrSubtract = str[0];
+  const nextIndex = findNextIndex(str);
+  const num = Number(str.slice(1, nextIndex));
+  return addOrSubtract === '+'
+    ? num + chronalCollaboration(str.slice(nextIndex))
+    : chronalCollaboration(str.slice(nextIndex)) - num;
+};
 
-module.exports = chronalCollaboration
+const findNextIndex = str => {
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === '+' || str[i] === '-') return i;
+  }
+  return str.length;
+};
+
+module.exports = chronalCollaboration;
