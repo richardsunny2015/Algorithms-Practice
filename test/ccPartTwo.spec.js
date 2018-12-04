f90ef95ed2b8307ef2db6a6ee45b5398f10cb8bd
Richard Lanchez <richardsunny2015@gmail.com>
2018-12-04 12:02:14 -0500

    Advent code day two part one #29 (#31)
    * chore(day2): Create test and function skeleton
    
    * test(day2): Finish writing tests for day2
    
    * feat(day2): Finish writing ims algrorithm
    
    * chore(tests): Add x's to describe code blocks
    
    * feat(day2): Write script that logs result of ims algo

--- test/ccPartTwo.spec.js
+++ test/ccPartTwo.spec.js
@@ -1,7 +1,7 @@
 const ccPartTwo = require('../advent-of-code/ccPartTwo')
 const {expect} = require('chai')
 
-describe('ccPartTwo', () => {
+xdescribe('ccPartTwo', () => {
     it('returns a number', () => {
         expect(ccPartTwo([+1, -1])).to.be.a('number')
     })
---
+++ test/day2.spec.js
@@ -0,0 +1,23 @@
+const {expect} = require('chai')
+const {ims} = require('../advent-of-code/day2')
+
+xdescribe('ims', () => {
+    it('returns a number', () => {
+        expect(ims(['aa', 'bbb'])).to.be.a('number')
+    })
+    it('returns the right number', () => {
+        expect(ims(['aa', 'bbb'])).to.equal(1)
+    })
+    it('can handle harder inputs', () => {
+        expect(ims([
+            'abcdef',
+            'bababc',
+            'abbcde',
+            'abcccd',
+            'aabcdd',
+            'abcdee',
+            'ababab'
+        ]))
+        .to.equal(12)
+    })
+})
