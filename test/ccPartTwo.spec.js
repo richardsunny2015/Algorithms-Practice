50cd9f2aa84de988725b721c9a127128157d1b79
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-04 08:41:41 -0500

    test(ccPartTwo): Finish writing tests

--- test/ccPartTwo.spec.js
+++ test/ccPartTwo.spec.js
@@ -3,6 +3,18 @@ const {expect} = require('chai')
 
 describe('ccPartTwo', () => {
     it('returns a number', () => {
-        expect(ccPartTwo(['+1', '-1'])).to.be.a('number')
+        expect(ccPartTwo([+1, -1])).to.be.a('number')
+    })
+    it('returns the correct number', () => {
+        expect(ccPartTwo([+1, -1])).to.equal(0)
+    })
+    it('can handle harder tests', () => {
+        expect(ccPartTwo([
+            +3,
+            +3,
+            +4,
+            -2,
+            -4
+        ])).to.equal(10)
     })
 })
