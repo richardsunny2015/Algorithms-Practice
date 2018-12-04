0dfd7a78b81843e6c1cf03095fb37be073777ef5
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-04 11:42:35 -0500

    test(day2): Finish writing tests for day2

--- test/day2.spec.js
+++ test/day2.spec.js
@@ -5,4 +5,19 @@ describe('ims', () => {
     it('returns a number', () => {
         expect(ims(['aa', 'bbb'])).to.be.a('number')
     })
+    it('returns the right number', () => {
+        expect(ims(['aa', 'bbb'])).to.equal(1)
+    })
+    it('can handle harder inputs', () => {
+        expect(ims([
+            'abcdef',
+            'bababc',
+            'abbcde',
+            'abcccd',
+            'aabcdd',
+            'abcdee',
+            'ababab'
+        ]))
+        .to.equal(12)
+    })
 })
