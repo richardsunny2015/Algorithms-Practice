a6f03677ecf08d49380392cae9b4884a41343b5e
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-04 07:34:12 -0500

    test(chronalCollaboration): Finish writing tests

--- test/chronalCollaboration.spec.js
+++ test/chronalCollaboration.spec.js
@@ -5,4 +5,13 @@ describe('chronalCollaboration', () => {
     it('returns a number', () => {
         expect(chronalCollaboration('')).to.be.a('number')
     })
+    it('can add positive numbers', () => {
+        expect(chronalCollaboration('+2+3+4')).to.equal(9)
+    })
+    it('can add negative numbers', () => {
+        expect(chronalCollaboration('-2-3-4')).to.equal((-9))
+    })
+    it('can add negative and positive numbers', () => {
+        expect(chronalCollaboration('-2+5-3+7')).to.equal(7)
+    })
 })
