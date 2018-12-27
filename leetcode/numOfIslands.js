010bc50277fd56ed7afc81d40d6a2e450da6bfe6
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-27 14:49:55 -0500

    doc(numOfIslands): Write documentation on how numOfIslands and dfs work

--- leetcode/numOfIslands.js
+++ leetcode/numOfIslands.js
@@ -4,7 +4,7 @@ const numOfIslands = grid => {
     for (let col = 0; col < grid[row].length; col++) {
       if (grid[row][col] === '1') {
         counter++;
-        dfs(grid, row, col);
+        dfs(grid, row, col); // start search at current land mass
       }
     }
   }
@@ -12,13 +12,14 @@ const numOfIslands = grid => {
 };
 
 const dfs = (grid, row, col) => {
+    // check for out of bounds
   if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) return;
   const value = grid[row][col];
   if (value === '1') {
-    grid[row][col] = '0';
-    dfs(grid, row - 1, col);
-    dfs(grid, row, col - 1);
-    dfs(grid, row + 1, col);
-    dfs(grid, row, col + 1);
+    grid[row][col] = '0'; // change value to 0 from 1
+    dfs(grid, row - 1, col); // check adjacent top
+    dfs(grid, row, col - 1); // check adjacent left
+    dfs(grid, row + 1, col); // check adjacent bottom
+    dfs(grid, row, col + 1); // check adjacent right
   }
 };
