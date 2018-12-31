703d8e9ab93b0af3531ada6d50519f8ec99b3f96
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-31 15:23:26 -0500

    feat(perms): Write perms function using list comprehension

--- algorithms-a-functional-approach-1999/perms.hs
+++ algorithms-a-functional-approach-1999/perms.hs
@@ -1,2 +1,7 @@
 module Perms where
 
+perms [] = [[]]
+perms xs = [x:p | x <- xs, p <- perms (removeFirst x xs)]
+    where removeFirst x []                 = []
+          removeFirst x (y:ys) | x == y    = ys
+                               | otherwise = y : removeFirst x ys
