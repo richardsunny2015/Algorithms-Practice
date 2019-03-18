09194631f6668370f214e50b361864823dc6a3bb
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-18 15:50:29 -0400

    feat(countingBits): Add implementation of counting bits solution

--- leetcode/countingBits.js
+++ leetcode/countingBits.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 /* Counting Bits
 Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
 
@@ -11,3 +12,19 @@ Input: 5
 Output: [0,1,1,2,1,2]
 */
 
+function countBits(num) {
+    let counts = []
+    for (let i = 0; i <= num; i++) {
+        counts.push(howManyOnes(i))
+    }
+    return counts
+}
+
+function howManyOnes(num) {
+    let count = 0
+    while (num > 0) {
+        count += num & 1
+        num >>= 1
+    }
+    return count
+}
