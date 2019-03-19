d33f2b1b6e3aeb254c752deb9427306b3c698eb6
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-19 16:12:36 -0400

    doc(hammingDistance): Write comments that explain how the implementation works as well as some explanations on bitwise operations

--- leetcode/hammingDistance.js
+++ leetcode/hammingDistance.js
@@ -20,11 +20,17 @@ Explanation:
 The above arrows point to positions where the corresponding bits are different. */
 
 function hammingDistance(x, y) {
-    let count = 0;
-    let xor = x ^ y
+    let count = 0; // inititate a count
+    let xor = x ^ y // find all bit in which the two numbers are different
+    /* XOR returns a 1 in a bit that one BUT NOT BOTH numbers
+        have 1 in. Example:
+            if you have a 5 and a 4, you will get 1 because:
+            5 = 101
+            4 = 100
+            1 = 001 */
     while (xor > 0) {
-        count += xor & 1
-        xor >>= 1
+        count += xor & 1 // add 1 to the count whenever the rightmost bit is a 1
+        xor >>= 1 // reassign xor to be current xor shifted 1 bit to the right
     }
     return count
 }
