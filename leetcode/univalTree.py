732f21886c7b7dcfb6cfbb698e81f3f35f39f527
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-28 19:12:02 -0400

    feat(univalTree): Add implementation of isUnivalTree algorithm

--- leetcode/univalTree.py
+++ leetcode/univalTree.py
@@ -21,3 +21,18 @@
 # The number of nodes in the given tree will be in the range [1, 100].
 # Each node's value will be an integer in the range [0, 99].
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        return self.helper(root, root.val)
+        
+    def helper(self, root, target):
+        if root is None: return True
+        elif root.val != target: return False
+        else: return self.helper(root.left, target) and self.helper(root.right, target)
