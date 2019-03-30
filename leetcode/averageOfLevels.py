fb148196a1a7f606da8fd2b8d161f1e4b4a25088
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-30 09:46:00 -0400

    feat(averageOfLevels): Implement solution to problem

--- leetcode/averageOfLevels.py
+++ leetcode/averageOfLevels.py
@@ -10,3 +10,22 @@
 # Explanation:
 # The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
 
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        results = []
+        queue = [root]
+        while len(queue) > 0:
+            average = self.average(list(map(lambda x: x.val, queue)))
+            results.append(average)
+            currLen = len(queue)
+            for i in range(currLen):
+                curr = queue.pop(0)
+                if curr.left is not None:
+                    queue.append(curr.left)
+                if curr.right is not None:
+                    queue.append(curr.right)
+        return results
+        
+    def average(self, nums):
+        print(nums)
+        return sum(nums) / len(nums)
