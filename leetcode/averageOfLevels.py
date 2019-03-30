1a27929cbcdb16fecc60bb25c4bbd97abcaec42f
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-30 09:56:26 -0400

    doc(averageOfLevels): Write comments that describe the though process of the implementation

--- leetcode/averageOfLevels.py
+++ leetcode/averageOfLevels.py
@@ -12,20 +12,29 @@
 
 class Solution:
     def averageOfLevels(self, root: TreeNode) -> List[float]:
-        results = []
-        queue = [root]
-        while len(queue) > 0:
-            average = self.average(list(map(lambda x: x.val, queue)))
-            results.append(average)
-            currLen = len(queue)
-            for i in range(currLen):
-                curr = queue.pop(0)
-                if curr.left is not None:
-                    queue.append(curr.left)
-                if curr.right is not None:
+        results = [] # list of averages that we will eventually return
+        queue = [root] # place root inside of our queue initially
+        while len(queue) > 0: # we will loop until the queue is empty
+            # EXPLANATION: The queue at the start of the current loop
+            # is a level of the binary tree. If the queue has 4 elements,
+            # that means that the current level of the binary tree has 4 nodes.
+            # Which means we can calculate the average at the start of each loop.
+            # After we append the average, we will pop off all the elements in the current queue.
+            # As we are popping off elements, we are also appending the children of each element.
+            # Repeat until there are no more nodes.
+
+
+            average = self.average(list(map(lambda x: x.val, queue))) # calculate average on every
+            # item in queue
+            results.append(average) # append the calculated average to results
+            currLen = len(queue) # find the length of queue
+            for i in range(currLen): # loop currLen times
+                curr = queue.pop(0) # pop off the first element of queue
+                if curr.left is not None: # if there is a left node
+                    queue.append(curr.left) # append left node
+                if curr.right is not None: # same as left
                     queue.append(curr.right)
         return results
         
-    def average(self, nums):
-        print(nums)
+    def average(self, nums): # helper function
         return sum(nums) / len(nums)
