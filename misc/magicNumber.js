558b292007e1c9437722bb010e392c4fb5cc7503
richardsunny2015 <richardsunny2015@gmail.com>
2019-04-04 15:08:35 -0400

    doc(magicNumber): Add documentation explaining magic number problem

---
+++ misc/magicNumber.js
@@ -0,0 +1,13 @@
+/* Given a number, sum the square of all the digits and repeat until
+   you get the original number again. Return the length of that sequence.
+   For instance: magicNumber(16) = 9 because:
+     1: 16
+     2: 1^2 + 6^2 = 37
+     3: 3^2 + 7^2 = 58
+     4: 5^2 + 8^2 = 89
+     5: 8^2 + 9^2 = 145
+     6: 1^2 + 4^2 + 5^2 = 42
+     7: 4^2 + 2^2 = 20
+     8: 2^2 + 0^2 = 4
+     9: 4^2 = 16 */
+
