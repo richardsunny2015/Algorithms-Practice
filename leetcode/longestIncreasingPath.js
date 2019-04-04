ca9acaf0a520676c19a4956f92bcd1edb1285082
richardsunny2015 <richardsunny2015@gmail.com>
2019-04-04 08:13:59 -0400

    feat(longestIncreasingPath): Add implementation of longestIncreasingPath solution

--- leetcode/longestIncreasingPath.js
+++ leetcode/longestIncreasingPath.js
@@ -22,3 +22,35 @@ Input: nums =
 ]
 Output: 4
 Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed. */
+
+const longestIncreasingPath = (matrix) => {
+    if (!matrix.length) return 0;
+    let cache = []
+    for (let i = 0; i < matrix.length; i++) {
+        const row = (new Array(matrix[i].length)).fill(0)
+        cache.push(row)
+    }
+    let max = -Infinity;
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            max = Math.max(max, dfs(-Infinity, row, col, matrix, cache));
+        }
+    }
+    console.log(cache)
+    return max;
+};
+
+const dfs = (target, row, col, matrix, cache) => {
+    if (row < 0
+        || col < 0
+        || row >= matrix.length
+        || col >= matrix[row].length
+        || matrix[row][col] <= target) return 0;
+    const curr = matrix[row][col];
+    if (cache[row][col]) return cache[row][col];
+    cache[row][col] = 1 + Math.max(dfs(curr, row - 1, col, matrix, cache)
+                       , dfs(curr, row, col - 1, matrix, cache)
+                       , dfs(curr, row + 1, col, matrix, cache)
+                       , dfs(curr, row, col + 1, matrix, cache));
+    return cache[row][col];
+};
