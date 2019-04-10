d9110fa4cd380134c4e8b735687d71ec9713b188
richardsunny2015 <richardsunny2015@gmail.com>
2019-04-10 08:39:31 -0400

    doc: Add documentation of how implementation works for reverse words with spaces problem

--- leetcode/reverseWordsWithSpaces.js
+++ leetcode/reverseWordsWithSpaces.js
@@ -25,33 +25,33 @@ You need to reduce multiple spaces between two words to a single space in the re
  */
 
 var reverseWords = function(s) {
-    const arr = buildArr(s);
-    let head = 0,
+    const arr = buildArr(s); // use helper to build arr
+    let head = 0, // instantiate head and tail
         tail = arr.length - 1;
-    while (head < tail) {
-        let temp = arr[head];
-        arr[head] = arr[tail];
-        arr[tail] = temp;
+    while (head < tail) { // loop til head is equal or greater than tail
+        let temp = arr[head]; // save our head in a temporary variable
+        arr[head] = arr[tail]; // make tail our new head
+        arr[tail] = temp; // make tail our temp
         head++;
         tail--;
     }
-    return arr.join(' ');
+    return arr.join(' '); // join the array with spaces
 };
 
 function buildArr(s) {
-    let arr = [],
+    let arr = [], // instantiate array and i
         i = 0;
     while (i < s.length) {
-        if (s[i] !== ' ') {
-            let word = '';
-            for (let j = i; j < s.length; j++) {
-                if (s[j] === ' ') break;
-                else word += s[j];
-                i = j
+        if (s[i] !== ' ') { // any time there is no space
+            let word = ''; // instantiate a word that we will push into array
+            for (let j = i; j < s.length; j++) { // loop til end of word OR
+                if (s[j] === ' ') break; // til we reach a space
+                else word += s[j]; // add letter to word if there's no space
+                i = j // set our i equal to j so that we don't revisit the same character
             }
             arr.push(word);
         }
-        i++;
+        i++; // always increment i
     }
     return arr;
 }
