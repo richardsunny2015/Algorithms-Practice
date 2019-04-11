f6e289c033383dbbb30130fe8f4016498fbcf103
richardsunny2015 <richardsunny2015@gmail.com>
2019-04-11 12:50:31 -0400

    doc: Add comments describing implementation

--- leetcode/maxIncreaseKeepingSkyline.js
+++ leetcode/maxIncreaseKeepingSkyline.js
@@ -41,29 +41,36 @@ they are a 1 x 1 x grid[i][j] rectangular prism.
 */
 
 function maxIncreaseKeepingSkyline(grid) {
+    /* build your arrays of max heights for each row
+    and each column. */
     const [maxRows, maxCols] = maxHeights(grid);
-    let sum = 0;
-    for (let row = 0; row < grid.length; row++) {
+    let sum = 0; // instantiate sum
+    for (let row = 0; row < grid.length; row++) { // loop through grid
         for (let col = 0; col < grid.length; col++) {
-            const building = grid[row][col],
+            /* The reason we calculate the min is because the building
+            can never be greater than the minimum of the tallest building
+            for that row and that column. If it were to be greater, it would
+            change how the skyline looks on one side of the grid. */
+            const building = grid[row][col], // current cell
                   min = Math.min(maxRows[row], maxCols[col]);
-            sum += min - building;
+            sum += min - building; /* add the difference of what we're increasing
+            the height to be and what the height is now */
         }
     }
     return sum;
 }
 
-function maxHeights(grid) {
-    const maxRows = (new Array(grid.length)).fill(0),
+function maxHeights(grid) { // helper function to find the max heights for rows and cols
+    const maxRows = (new Array(grid.length)).fill(0), // start our arrays with zeroes filled in
           maxCols = (new Array(grid.length)).fill(0);
     for (let row = 0; row < grid.length; row++) {
-        maxRows[row] = Math.max(...grid[row]);
+        maxRows[row] = Math.max(...grid[row]); // find the max value of that row
         for (let col = 0; col < grid.length; col++) {
-            maxCols[col] = Math.max(
-                maxCols[col],
-                grid[row][col]
+            maxCols[col] = Math.max( // find max of
+                maxCols[col], // the current max of this column and
+                grid[row][col] // the current building
             )
         }
     }
-    return [maxRows, maxCols]
+    return [maxRows, maxCols] // return tuple
 }
