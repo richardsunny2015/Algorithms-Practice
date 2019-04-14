2de549b27b70b5a7393f56deefa5aaf117f19174
richardsunny2015 <richardsunny2015@gmail.com>
2019-04-14 16:08:32 -0400

    feat: Write implementation of singleNonDuplicate algorithm

--- leetcode/singleNonDuplicate.js
+++ leetcode/singleNonDuplicate.js
@@ -11,3 +11,17 @@ Input: [3,3,7,7,10,11,11]
 Output: 10
 Note: Your solution should run in O(log n) time and O(1) space.
 */
+
+function singleNonDuplicate(nums) {
+    return helper(nums, 0, nums.length - 1);
+}
+
+function helper(nums, lower, upper) {
+    const mid = Math.floor((upper - lower) / 2) + lower;
+    const current = nums[mid];
+    if (current !== nums[mid - 1] && current !== nums[mid + 1]) return current;
+    return (mid % 2 === 0 && current === nums[mid - 1])
+        || (mid % 2 !== 0 && current !== nums[mid - 1])
+            ? helper(nums, lower, mid - 1)
+            : helper(nums, mid + 1, upper);
+}
