3292c16c89741e34ad4e9143b991ca2d299a5cdd
richardsunny2015 <richardsunny2015@gmail.com>
2019-04-14 16:16:53 -0400

    doc: Write comments explaining implementation

--- leetcode/singleNonDuplicate.js
+++ leetcode/singleNonDuplicate.js
@@ -13,15 +13,28 @@ Note: Your solution should run in O(log n) time and O(1) space.
 */
 
 function singleNonDuplicate(nums) {
-    return helper(nums, 0, nums.length - 1);
+    return helper(nums, 0, nums.length - 1); // use helper, pass in 0 and the last index as
+    // starting values
 }
 
 function helper(nums, lower, upper) {
-    const mid = Math.floor((upper - lower) / 2) + lower;
-    const current = nums[mid];
+    const mid = Math.floor((upper - lower) / 2) + lower; // calculate mid index
+    const current = nums[mid]; // set current
+    // if current doesn't equal to the numbers, next to it, you found it
     if (current !== nums[mid - 1] && current !== nums[mid + 1]) return current;
-    return (mid % 2 === 0 && current === nums[mid - 1])
-        || (mid % 2 !== 0 && current !== nums[mid - 1])
-            ? helper(nums, lower, mid - 1)
-            : helper(nums, mid + 1, upper);
+    return (mid % 2 === 0 && current === nums[mid - 1]) /* if the index is even
+    and the current is equal to the previous number OR */
+        || (mid % 2 !== 0 && current !== nums[mid - 1]) /* if the index is odd
+        and the current is not equal to the previous number */
+            ? helper(nums, lower, mid - 1) // set upper to mid minus 1
+            : helper(nums, mid + 1, upper);// else set lower to mid plus 1
 }
+
+/*
+Some clarification:
+    Given this example: [1,1,2,2,3,3,4,5,5]
+        We can see that if the mid, in this case '4', is even, and that the number above it
+        is the same, that means that all the previous numbers are duplicates.
+        If the mid was even and the previous number was the same as mid, the singleton would
+        be in the first half of the nums array. The logic is switched when the mid is odd.
+*/
